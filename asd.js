cb044d0852be494c1ede4779e2808d32d6c48ff8
astral-raze <144711184+astral-raze@users.noreply.github.com>
2024-11-18 02:45:07 +0500

    Update asd.js

--- asd.js
+++ asd.js
@@ -1,1 +1,1 @@
-{"arizona":[{"number":1,"name":"arena","ip":"194.147.32.61","port":7777,"online":0,"maxplayers":100,"password":true,"vk":"","tg":"","inst":"","icon":"https://arizona-recovery.react.domains/desktop/resources/icons/arizona/1.png"}]}
+{"arizona":[{"number":1,"name":"arena","ip":"194.147.32.61","port":7777,"online":0,"maxplayers":666,"password":true,"vk":"","tg":"","inst":"","icon":"https://arizona-recovery.react.domains/desktop/resources/icons/arizona/1.png"}]}
